41785790bfab22c79d717bdd5a687e583096824f
kirussssa <kindinamarina@gmail.com>
2025-04-29 23:07:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Скрипт для обработки покупки курса
+
+document.getElementById('buy-button').addEventListener('click', function() {
+    alert('Спасибо за покупку! Инструкции отправлены на ваш email.');
+});
